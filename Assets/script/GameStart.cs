d51cede1c4c7c380d723f8da3729b25e65dd54a8
ryoneda <ryo.roma.16@gmail.com>
2020-08-19 19:00:55 +0900

    Connect MySQL

--- Assets/script/GameStart.cs
+++ Assets/script/GameStart.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;                   // Exception
+using MySql.Data;               // MySQL関連
+using MySql.Data.MySqlClient;
 
 
 public class GameStart : MonoBehaviour
 {
+
     // Start is called before the first frame update
     public void Onclick(){
        SceneManager.LoadScene("WCST");
-	}
+
+       MySqlConnection con = new MySqlConnection("Server =127.0.0.1; UserID =root; Password =experiment; Database =test");
+        
+        try
+        {
+            con.Open();
+            string sql = "Select * from user";
+            MySqlCommand com = new MySqlCommand(sql, con);
+            MySqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                Debug.Log(reader[0]);
+                 Debug.Log("Done");
+            }
+            reader.Close();
+        }catch(MySqlException e)
+        {
+            Debug.Log(e.ToString());
+        }
+        
+        Debug.Log("");
+    }
 }
