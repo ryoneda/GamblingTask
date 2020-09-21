ed908ac56dea5b21bc63c2a1f69359486e380d16
ryoneda <ryo.roma.16@gmail.com>
2020-09-21 17:44:01 +0900

    add HRV

---
+++ Assets/script/ChangeFace.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeFace : MonoBehaviour
+{
+SpriteRenderer MainSpriteRenderer;
+    double sdnn;
+    double baseline;
+    double max;
+    public Sprite Normal;
+    public Sprite Smile1;
+    public Sprite Smile2;
+    public Sprite Smile3;
+    public Sprite Smile4;
+    public Sprite Smile5;
+    public Sprite Disgust1;
+    public Sprite Disgust2;
+    public Sprite Disgust3;
+    public Sprite Disgust4;
+    public Sprite Disgust5;
+
+
+    void Start ()
+    {
+       // このobjectのSpriteRendererを取得
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+        void Update()
+    {
+        sdnn = GameObject.Find ("Database").GetComponent<ToMySQL>().sdnn;
+        baseline = GameObject.Find ("Database").GetComponent<ToMySQL>().baseline;
+        max = GameObject.Find ("Database").GetComponent<ToMySQL>().max;
+        float score=(float)((sdnn-baseline)/(max-baseline))*100;
+
+        Change(score);
+        Debug.Log(score);
+        
+    }
+
+    // 何かしらのタイミングで呼ばれる
+    void Change(float score)
+    {
+       if(80<=score){
+           MainSpriteRenderer.sprite = Smile5;
+	   }else if(60<=score && score<80){
+           MainSpriteRenderer.sprite = Smile4;
+	   }else if(40<=score && score<60){
+           MainSpriteRenderer.sprite = Smile3;
+	   }else if(20<=score && score<40){
+           MainSpriteRenderer.sprite = Smile2;
+	   }else if(0<=score && score<20){
+           MainSpriteRenderer.sprite = Smile1;
+	   }else if(-20<=score && score<0){
+           MainSpriteRenderer.sprite = Disgust1;
+	   }else if(-40<=score && score<-20){
+           MainSpriteRenderer.sprite = Disgust2;
+	   }else if(-60<=score && score<-40){
+           MainSpriteRenderer.sprite = Disgust3;
+	   }else if(-80<=score  && score<-60){
+           MainSpriteRenderer.sprite = Disgust4;
+	   }else if(score<-80){
+           MainSpriteRenderer.sprite = Disgust5;
+	   }
+       // SpriteRenderのspriteを設定済みの他のspriteに変更
+       // 例) HoldSpriteに変更
+       if (Input.GetKey(KeyCode.Space)) {
+            MainSpriteRenderer.sprite = Smile1;
+        }
+    }
+}
--- Assets/script/PitchChanger.cs
+++ Assets/script/PitchChanger.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PitchChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    AudioSource audioSource;
+    float maxPitch=1.5f;
+    float minPitch=0.5f;
+    float startingPitch = 1;
+    double sdnn;
+    double baseline;
+    double max;
+    float score;
+
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = startingPitch;
     }
 
     // Update is called once per frame
     void Update()
     {
+        sdnn = GameObject.Find ("Database").GetComponent<ToMySQL>().sdnn;
+        baseline = GameObject.Find ("Database").GetComponent<ToMySQL>().baseline;
+        max = GameObject.Find ("Database").GetComponent<ToMySQL>().max;
         
+        //score=(float)((sdnn)/(max))*1.5f;  
+        score=(float)(sdnn/baseline);
+		
+        audioSource.pitch = score;
     }
 }
